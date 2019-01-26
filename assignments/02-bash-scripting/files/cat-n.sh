ca3f2a2363c650fbd4e07632ba17d1a2f11bcd2c
Your Name <you@example.com>
2019-01-26 09:48:21 -0700

    homework 2 started

---
+++ assignments/02-bash-scripting/files/cat-n.sh
@@ -0,0 +1,14 @@
+####this is cat-n.sh####
+####it mimics the output of the command cat -n ####
+
+#### If there are no arguments it should print a "Usage Statement" and
+#### exit with an error code (greater than zero)
+
+#### Program will expect to recieve an argument in $1
+
+#### If the argument is not a file it should notify the user and exit with
+#### a different error code
+
+#### It will iterate over the lines in the files and print
+######## the line number, a space , and line from the file
+
