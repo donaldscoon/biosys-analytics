102e3e735a41ead9903e75a1a26b945e3dc42e85
Donald Coon <donaldscoon@gmail.com>
2019-01-29 08:59:12 -0700

    homework 2 done!

--- assignments/02-bash-scripting/cat-n.sh
+++ assignments/02-bash-scripting/cat-n.sh
@@ -1,25 +1,26 @@
+#!/usr/bin/env bash
+ 
 ####this is cat-n.sh####
 ####it mimics the output of the command cat -n ####
 
-#!/usr/bin/env
 
 #### If there are no arguments it should print a Usage Statement and
 #### exit with an error code (greater than zero)
 
 if [[ $# -eq 0 ]]; then
-	echo "Usage: cat-n.sh ARG"
+	echo "Usage: cat-n.sh FILE"
 	exit 1
 fi
 
 #### Program will expect to recieve an argument in $1
 
-ARG=$1
+FILE=$1
 
 #### If the argument is not a file it should notify the user and exit with
 #### a different error code
 
-if [[ ! -f "$ARG" ]]; then
-        echo "ARG is NOT a file"
+if [[ ! -f "$FILE" ]]; then
+        echo "$FILE is not a file"
 	exit 2
 fi
 
@@ -29,6 +30,6 @@ i=0
 FILE=$1
 while read -r LINE; do
 	i=$((i+1))
-	echo $i $LINE
+	echo $i "$LINE"
 done < "$FILE"
 
--- assignments/02-bash-scripting/head.sh
+++ assignments/02-bash-scripting/head.sh
@@ -1,28 +1,27 @@
+#!/usr/bin/env bash
 
 ####this is head.sh####
 ####it mimics the output of the command head####
 
 
-#!/c/Users/Donald
-
 #### If there are no arguments it should print a Usage Statement and
 #### exit with an error code (greater than zero)
 
 if [[ $# -eq 0 ]]; then
-        echo "Usage: head.sh ARG LINES"
+        echo "Usage: head.sh FILE [LINES]"
         exit 1
 fi
 
 #### Program will expect to recieve an argument in $1 and maybe in $2
 
-ARG=$1
+FILE=$1
 LINES=${2:-3}
 
 #### If the argument is not a file it should notify the user and exit with
 #### a different error code
 
-if [[ ! -f "$ARG" ]]; then
-        echo "ARG is NOT a file"
+if [[ ! -f "$FILE" ]]; then
+        echo "$FILE is not a file"
         exit 2
 fi
 
@@ -33,7 +32,7 @@ fi
 FILE=$1
 
 while read -r LINE; do
-        echo $LINE
+        echo "$LINE"
         i=$((i+1))
         if [[ $i -eq LINES ]]; then
                 break
