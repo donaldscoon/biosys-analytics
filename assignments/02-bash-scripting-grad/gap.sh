f325f4fba6ab1bb2d93f0bdab9494174ca2dbfff
Your Name <you@example.com>
2019-01-30 16:46:17 -0700

    might have finished gap.sh

--- assignments/02-bash-scripting-grad/gap.sh
+++ assignments/02-bash-scripting-grad/gap.sh
@@ -5,21 +5,34 @@
 
 #### Usage statement
 
-if [[ $1 -eq "--help" ]]; then
-        echo "Usage: gap.sh '[SEARCH_TERMS]'"
-	echo "SEARCH_TERMS uses regular expressions"
-        exit 1
-fi
+#if [[ $1 -eq -help ]]; then
+#       echo "Usage: gap.sh SEARCH_TERMS"
+#	echo "SEARCH_TERMS uses regular expressions"
+#        exit 1
+#fi
 
 if [[ $# -gt 1 ]]; then
-        echo "Usage: gap.sh '[SEARCH_TERMS]'"
+        echo "Usage: gap.sh SEARCH_TERMS"
         exit 2
 fi
+
+#### last error message
+#if [[ $1 -eq q ]]; then
+#      echo "There are no countries that start with $SEARCH_TERMS"
+#      exit 3
+#fi
 
 #### Variable Assignment
 SEARCH_TERMS=$1
 TMP_FILE=$(mktemp)
 TMP_SORTED=$(mktemp)
+
+#### last error message
+#if [[ $1 -eq "q" ]]; then
+ #     echo "There are no countries that start with $SEARCH_TERMS"
+  #    exit 1
+#fi
+
 
 #### Output files contents into temp file
 ls ../../data/gapminder > $TMP_FILE
@@ -30,6 +43,8 @@ grep -i "^$SEARCH_TERMS" $TMP_FILE > $TMP_SORTED
 #### remove .cc.txt
 cat $TMP_SORTED | cut -d '.' -f 1
 
-
-
+if [[ $1 -eq "q" ]]; then
+      echo "There are no countries that start with $SEARCH_TERMS"
+      exit 1
+fi
 
