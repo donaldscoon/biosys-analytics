d8e8c133507673c6e2a7b1e776d93caa1e44c5b6
Your Name <you@example.com>
2019-01-30 12:21:54 -0700

    grep works

--- assignments/02-bash-scripting-grad/gap.sh
+++ assignments/02-bash-scripting-grad/gap.sh
@@ -6,13 +6,13 @@
 #### Usage statement
 
 if [[ $1 -eq "--help" ]]; then
-        echo "Usage: gap.sh [SEARCH_TERMS]"
+        echo "Usage: gap.sh '[SEARCH_TERMS]'"
 	echo "SEARCH_TERMS uses regular expressions"
         exit 1
 fi
 
 if [[ $# -gt 1 ]]; then
-        echo "Usage: gap.sh [SEARCH_TERMS]"
+        echo "Usage: gap.sh '[SEARCH_TERMS]'"
         exit 2
 fi
 
@@ -22,14 +22,14 @@ SEARCH_TERMS=$1
 #### temporary file for manipulating search results
 mktemp FILES_LIST.tmp.XXX -q
 mktemp SEARCH_RESULTS.tmp.XXX -q
+
 #### Output files contents into temp file
 ls ../../data/gapminder > FILES_LIST.tmp.*
 
+#### remove .cc.txt
+
 #### Sort and filter list of countries according to user input
-grep -i '$SEARCH_TERMS' FILES_LIST.tmp.* > SEARCH_RESULTS.tmp.*
-
-cat SEARCH_RESULTS.tmp.*
-
+grep -i "$SEARCH_TERMS" FILES_LIST.tmp.* > SEARCH_RESULTS.tmp.*
 
 #### Removing the temp files
 rm *.tmp.*
