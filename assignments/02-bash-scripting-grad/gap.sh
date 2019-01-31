1acf6cf1f579ed8c7d26dc57dda08cd1f7d02a06
Donald Coon <donaldscoon@gmail.com>
2019-01-31 09:25:38 -0700

    BARELY FINSHED I WIN!!!

--- assignments/02-bash-scripting-grad/gap.sh
+++ assignments/02-bash-scripting-grad/gap.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 ####this is gap.sh####
@@ -5,34 +6,15 @@
 
 #### Usage statement
 
-#if [[ $1 -eq -help ]]; then
-#       echo "Usage: gap.sh SEARCH_TERMS"
-#	echo "SEARCH_TERMS uses regular expressions"
-#        exit 1
-#fi
-
 if [[ $# -gt 1 ]]; then
         echo "Usage: gap.sh SEARCH_TERMS"
-        exit 2
+        exit 1
 fi
-
-#### last error message
-#if [[ $1 -eq q ]]; then
-#      echo "There are no countries that start with $SEARCH_TERMS"
-#      exit 3
-#fi
 
 #### Variable Assignment
 SEARCH_TERMS=$1
 TMP_FILE=$(mktemp)
 TMP_SORTED=$(mktemp)
-
-#### last error message
-#if [[ $1 -eq "q" ]]; then
- #     echo "There are no countries that start with $SEARCH_TERMS"
-  #    exit 1
-#fi
-
 
 #### Output files contents into temp file
 ls ../../data/gapminder > $TMP_FILE
@@ -40,11 +22,13 @@ ls ../../data/gapminder > $TMP_FILE
 #### Sort and filter list of countries according to user input
 grep -i "^$SEARCH_TERMS" $TMP_FILE > $TMP_SORTED
 
+NUM_FILES=$(wc -l "$TMP_SORTED" |awk '{print $1}')
+
+if [[ $NUM_FILES -lt 1 ]]; then
+      echo "There are no countries starting with \"$SEARCH_TERMS\""
+      exit 0
+fi
+
 #### remove .cc.txt
 cat $TMP_SORTED | cut -d '.' -f 1
-
-if [[ $1 -eq "q" ]]; then
-      echo "There are no countries that start with $SEARCH_TERMS"
-      exit 1
-fi
 
