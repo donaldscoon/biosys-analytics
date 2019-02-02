9c62c61ecc1b4c07fa539c22c222b0eb47820b5c
Donald Coon <donaldscoon@gmail.com>
2019-02-02 15:33:19 -0700

    counter works needs print functions

--- assignments/03-python-hello/vowel_counter.py
+++ assignments/03-python-hello/vowel_counter.py
@@ -8,19 +8,52 @@ Purpose: Rock the Casbah
 import os
 import sys
 
+args = sys.argv[1:]
+
+#-------------------------------------------------
+
+if len(args) != 1:
+    print('Usage: {} DNA'.format(os.path.basename(sys.argv[0])))
+    sys.exit(1)
+
+word = args[0]
+
+count = 0
+
+for letter in word.lower():
+    if letter == 'a':
+        count += 1
+    elif letter == 'e':
+        count += 1
+    elif letter == 'i':
+        count += 1
+    elif letter == 'o':
+        count += 1
+    elif letter == 'u':
+        count += 1
+
+if count == 5:
+    print(count)
+
+#---------------------------------------------------
 
 # --------------------------------------------------
-def main():
-    args = sys.argv[1:]
-
-    if len(args) != 1:
-        print('Usage: {} ARG'.format(os.path.basename(sys.argv[0])))
-        sys.exit(1)
-
-    arg = args[0]
-
-    print('Arg is "{}"'.format(arg))
-
-
+#def main():
+#    args = sys.argv[1:]
+#    count = 0
+#
+#    if len(args) != 1:
+#        print('Usage: {} ARG'.format(os.path.basename(sys.argv[0])))
+#        sys.exit(1)
+#
+#    word = args[0]
+#
+#    for letter in word:
+#        if letter == 'a' or letter == 'A':
+#            count += 1
+#
+#    print(str('count'))
+#
+#
 # --------------------------------------------------
-main()
+#main()
