0d0d00ff5e95fa70013766853ab02d1e5c6a7277
Donald Coon <donaldscoon@gmail.com>
2019-02-04 15:08:23 -0700

    bad input message generated

--- assignments/03-python-grad/grid.py
+++ assignments/03-python-grad/grid.py
@@ -12,12 +12,13 @@ import sys
 # --------------------------------------------------
 def main():
     args = sys.argv[1:]
-
+    arg = int(args[0])
+    
     if len(args) != 1:
         print('Usage: grid.py NUM'.format(os.path.basename(sys.argv[0])))
         sys.exit(1)
-    if (args) < 2:
-        print('NUM ' + (args) + ' must be between 1 and 9')
+    if (arg) < 2:
+        print('NUM (' + str((arg)) + ') must be between 1 and 9')
         sys.exit(1)
 
     arg = args[0]
