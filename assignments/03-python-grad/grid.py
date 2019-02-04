4a0f0edb22371dd8cf8aac69e7669c534e3f4d08
Donald Coon <donaldscoon@gmail.com>
2019-02-04 15:17:40 -0700

    all bad inputs completed

--- assignments/03-python-grad/grid.py
+++ assignments/03-python-grad/grid.py
@@ -12,19 +12,22 @@ import sys
 # --------------------------------------------------
 def main():
     args = sys.argv[1:]
-    arg = int(args[0])
     
+
     if len(args) != 1:
         print('Usage: grid.py NUM'.format(os.path.basename(sys.argv[0])))
         sys.exit(1)
-    if (arg) < 2:
-        print('NUM (' + str((arg)) + ') must be between 1 and 9')
+
+    grid_size = int(args[0])
+
+    if (grid_size) < 2:
+        print('NUM (' + str((grid_size)) + ') must be between 1 and 9')
+        sys.exit(1)
+    if (grid_size) > 9:
+        print('NUM (' + str((grid_size)) + ') must be between 1 and 9')
         sys.exit(1)
 
-    arg = args[0]
-
-    print('Arg is "{}"'.format(arg))
-    print(arg)
+    
 
 # --------------------------------------------------
 main()
