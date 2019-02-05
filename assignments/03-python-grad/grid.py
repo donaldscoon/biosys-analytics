efbac1499ffcc918484885bb9d734799257900be
Donald Coon <donaldscoon@gmail.com>
2019-02-05 10:53:02 -0700

    please work

--- assignments/03-python-grad/grid.py
+++ assignments/03-python-grad/grid.py
@@ -26,8 +26,74 @@ def main():
     if (grid_size) > 9:
         print('NUM (' + str((grid_size)) + ') must be between 1 and 9')
         sys.exit(1)
-
     
+# ----------------------------------------------------------------------------
+#### grid sizes
+    
+    count = 0
+
+    if (grid_size) == 2:
+        final = 4
+        rows = 2
+    if (grid_size) == 3:
+        final = 9
+        rows = 3
+    if (grid_size) == 4:
+        final = 16
+        rows = 4
+    if (grid_size) == 5:
+        final = 25
+        rows = 5
+    if (grid_size) == 6:
+        final = 36
+        rows = 6
+    if (grid_size) == 7:
+        final = 42
+        rows = 7
+    if (grid_size) == 8:
+        final = 64
+        rows = 8
+    if (grid_size) == 9:
+        final = 81
+        rows = 9
+
+    print(final)
+    print(rows)
+
+#### perhaps similar structure to hello print, but with rows?
+#### curly brackets can be used to call lists
+    
+    testgrid = [1, 2, 3, 4]
+    print(testgrid[:2])
+    print(testgrid[2:])
+
+
 
 # --------------------------------------------------
-main()
+main() 
+
+""" 
+names = ['Larry', 'Moe', 'Curly', 'Shemp'
+
+for name in names:
+	print(i + 1, name)
+	i += 1
+ 
+for i, name in enumerate(names):   #### gives position and name useful for later hw
+	print(i + 1, name)
+
+
+"""
+"""
+num = 2
+num ** 2
+range(num ** 2)
+for i in range(num ** 2):
+	print(i)
+
+for i in range(1, num ** 2 + 1):
+	print(i)
+
+for i in range(, num ** 2 +1 ):
+	print('{:3}'.format(i), end='')
+"""
