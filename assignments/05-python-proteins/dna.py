e2416ffb510b696550da138dd0de7d115be7fc86
Donald Coon <donaldscoon@gmail.com>
2019-02-14 10:46:51 -0700

    saving for use later

---
+++ assignments/05-python-proteins/dna.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Author : donaldscoon
+Date   : 2019-02-14
+Purpose: Rock the Casbah
+"""
+
+import os
+import sys
+
+
+# --------------------------------------------------
+def main():
+    args = sys.argv[1:]
+
+    if len(args) != 1:
+        print('Usage: {} ARG'.format(os.path.basename(sys.argv[0])))
+        sys.exit(1)
+
+    DNA = args[0]
+
+    print('DNA is "{}"'.format(DNA))
+
+    counts = {}   #making a dictionary
+    for char in DNA:
+       print(char)
+       if char not in counts:
+          counts[char] == 0
+
+       counts[char] += 1
+
+    print('{} {} {} {}'.format(counts.get('A')))
+
+# --------------------------------------------------
+main()
