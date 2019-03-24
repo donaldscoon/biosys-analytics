a03e8241f1602ea0bdb2be8f0b4a63853beb5c95
Donald <donaldscoon@gmail.com>
2019-03-23 17:01:21 -0700

    finished! hopefully, need randomness fixed

--- assignments/10-war/war.py
+++ assignments/10-war/war.py
@@ -20,12 +20,7 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument(
-        '-s',
-        '--seed',
-        help='used for testing',
-        metavar='int',
-        type=int,
-        default=None)
+        '-s', '--seed', help='activates seed', action='store_true')
 
     return parser.parse_args()
 
@@ -48,12 +43,14 @@ def main():
     """Dizzy Atmosphere by Dizzy Gillespie"""
     args = get_args()
     seed = args.seed
-    random.seed(seed)
+
+    """ADD RANDOM SEED STUFF HERE"""
 
     cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     suite = ['♥', '♠', '♣', '♦']
+    # value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14']
 
-    deck = list(product(suite, cards))
+    deck = sorted(product(suite, cards))
     shuffle = random.shuffle(deck)
     player1_wins = 0
     player2_wins = 0
@@ -63,26 +60,45 @@ def main():
         player1 = deck.pop(0)
         player1_card = player1[1]
         player1_suite = player1[0]
+        player1_value = player1[1]
+        if player1_value == 'J':
+            player1_value = 11
+        if player1_value == 'Q':
+            player1_value = 12
+        if player1_value == 'K':
+            player1_value = 13
+        if player1_value == 'A':
+            player1_value = 14
         player1_hand = player1_suite + player1_card
+
 
         """Ready Player 2?"""
         player2 = deck.pop(0)
         player2_card = player2[1]
         player2_suite = player2[0]
+        player2_value = player2[1]
+        if player2_value == 'J':
+            player2_value = 11
+        if player2_value == 'Q':
+            player2_value = 12
+        if player2_value == 'K':
+            player2_value = 13
+        if player2_value == 'A':
+            player2_value = 14
         player2_hand = player2_suite + player2_card
 
         """This means war"""
-        if player1_card > player2_card:
+        if int(player1_value) > int(player2_value):
             winner = ('P1')
-            print(' {:4}{:4}{:4}'.format(player1_hand, player2_hand, winner))
+            print('{:4}{:4}{:4}'.format(player1_hand, player2_hand, winner))
             player1_wins += 1
-        elif player1_card < player2_card:
+        elif int(player1_value) < int(player2_value):
             winner = ('P2')
-            print(' {:4}{:4}{:4}'.format(player1_hand, player2_hand, winner))
+            print('{:4}{:4}{:4}'.format(player1_hand, player2_hand, winner))
             player2_wins += 1
         else:
             winner = ('WAR!')
-            print(' {:4}{:4}{:4}'.format(player1_hand, player2_hand, winner))
+            print('{:4}{:4}{:4}'.format(player1_hand, player2_hand, winner))
 
     """and the winner is..."""
     if player1_wins > player2_wins:
