1bd1334b03555d4f1d2d9d555236b8dd6e177fe3
Donald <donaldscoon@gmail.com>
2019-04-01 15:31:44 -0700

    dates regex finished!

--- assignments/11-regex-dates/dates.py
+++ assignments/11-regex-dates/dates.py
@@ -31,9 +31,9 @@ arg = args[0]
 
 date_re = re.compile('(?P<year>\d{4})'     # YYYY
                      '[/-]?'               # optional seperator
-                     '(?P<month>\d{2})'    # MM
+                     '(?P<month>\d{1,2})'    # MM
                      '[-]?'               # optional seperator
-                     '(?P<day>\d{2})?')     # DD
+                     '(?P<day>\d{1,2})?')     # DD
 
 """ 2nd REGEX covers
 12/06
@@ -41,14 +41,14 @@ date_re = re.compile('(?P<year>\d{4})'     # YYYY
 
 date_re2 = re.compile('(?P<month>\d{1,2})'
                        '[/-]?'
-                       '(?P<year>\d{2})')
+                       '(?P<year>\d{4})')
 
 """ 3rd REGEX covers
 2/14
 2/14-12/15
 """
 
-date_re3 = re.compile('(?P<month>\d{1})'
+date_re3 = re.compile('(?P<month>\d{1,2})'
                       '[/-]'
                       '(?P<year>\d{1,2})')
 
@@ -59,26 +59,40 @@ Dec, 2015
 April, 2017
 """
 
-# date_re4 = re.compile('(?P<month>[Jan|January|Feb|February|Mar|March|Apr|April|May|Jun|June|Jul|July|Aug|August|Sep|September|Oct|October|Nov|November|Dec|December]{3})'
-#                       '[/,-]'
-#                       '([\s*])?'
-#                       '(?P<year>\d{4})')
-
+date_re4 = re.compile('(?P<month>[Jan|January|Feb|February|Mar|March|Apr|April|May|Jul|July|Aug|August|Sep|September|Oct|October|Nov|November|Dec|December]{1,9})'
+                      '([/,-])?'
+                      '(\s*)?'
+                      '(?P<year>\d{4}$)')
+# print(date_re)
+# print(date_re2)
+# print(date_re3)
 # print(date_re4)
-match = (date_re.match(arg)) or (date_re2.match(arg)) or (date_re3.match(arg))# or (date_re4.match(arg))
-# print(match)
 
-d_match = (match.groupdict())
+match = (date_re.match(arg)) or (date_re2.match(arg)) or (date_re3.match(arg)) or (date_re4.match(arg))
+# match = (date_re.match(arg))
+# match2 = (date_re2.match(arg))
+# match3 = (date_re3.match(arg))
+# match4 = (date_re4.match(arg))
+
+# print(match)
+# print(match2)
+# print(match3)
+# print(match4)
 
 if match == None:
     print('No match')
     sys.exit(0)
 
+d_match = (match.groupdict())
+
 if len(d_match) < 3:
     d_match['day'] = '01'
-    
+
 if d_match.get('day') == None:
     d_match['day'] = '01'
+
+if len(d_match.get('day')) < 2:
+    d_match['day'] = '0' + d_match.get('day')
 
 if len(d_match.get('year')) < 3:
     d_match['year'] = ('20' + d_match.get('year'))
@@ -86,7 +100,13 @@ if len(d_match.get('year')) < 3:
 if len(d_match.get('month')) < 2:
     d_match['month'] = ('0' + d_match.get('month'))
 
-# if d_match.get('month') == 'Jan':
-#     d_match['month'] = '01'
+d_months = {'Jan': '01', 'January': '01', 'Feb': '02', 'February': '02', 'Mar': '03', 'March': '03',
+            'Apr': '04', 'April': '04', 'May': '05', 'Jun': '06', 'June': '06', 
+            'Jul': '07', 'July': '07', 'Aug': '08', 'August': '08', 'Sep': '09', 'September': '09',
+            'Oct': '10', 'October': '10', 'Nov': '11', 'November': '11', 'Dec': '12', 'December': '12'}
+
+for month in d_months:
+    if d_match.get('month') == month:
+        d_match['month'] = (d_months.get(month))
 
 print('{}-{}-{}'.format(d_match.get('year'), d_match.get('month'), d_match.get('day')))
