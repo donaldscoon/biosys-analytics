47d15fe09cce7e9e94da13c3a78b2db681ddd594
Donald <donaldscoon@gmail.com>
2019-04-12 09:58:13 -0700

    readme incomplete?

--- assignments/13-grph/grph.py
+++ assignments/13-grph/grph.py
@@ -59,6 +59,52 @@ def main():
 
     if k_value < 1:
         die('-k "{}" must be a positive integer'.format(k_value))
+
+    k_value = 3
+    text = [line.strip() for line in open(input_file)]
+    i = 2
+    while i < len(text):
+        if not text[i].startswith('>') and not text[i-1].startswith('>'):
+            text[i-1] += text[i]
+            del text[i]
+            i -= 1
+        i += 1
+    dna = {}
+    for i in range(0,len(text),2):
+        dna[text[i][1:]] = text[i+1]
+    for first in dna.keys():
+        for second in dna.keys():
+            if first != second and dna[first][len(dna[first])-k_value:] == dna[second][0:k_value]:
+                print(first + " " + second)
+
+
+
+
+
+
+
+    # d_k_start = {}
+    # d_k_end = {}
+
+    # d_k_title_re = re.compile('(?P<title>Rosalind_\d{4})$')
+
+    # d_k_start_re = re.compile('(?P<start>^[ACGTacgt]{3})')
+
+    # d_k_end_re = re.compile('(?P<end>\D{3}$)')
+
+    # with open(input_file) as fh:
+    #     for line in fh:
+    #         print(line)
+            # match1 = d_k_title_re.search(line)
+            # d_match = match1.groupdict()
+            # print(d_match)
+
+        # d_match['month'] = (d_months.get(month))
+
+
+
+
+
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
